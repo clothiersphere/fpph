48abfc55ca9c68b0eb8b196ee6e10bb5ac72a79a
ChristopherLee <xchrislee@gmail.com>
2017-07-15 17:04:21 -0700

    Add salt bae place holder. file loader loaded

--- src/components/PlaceFood/presenter.js
+++ src/components/PlaceFood/presenter.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 var PlaceFood = () => {
   return (
-    <div>
-      <h1>PASTA!</h1>
+    <div className="mainContainer">
+      <img src={require(`../../../dist/img/placeholder.jpg`)} height='50%' width='50%'/>
     </div>
   )
 }
