8d30bc6b76436410538c82fd2bec1bc003c294cf
ChristopherLee <xchrislee@gmail.com>
2017-07-16 17:13:09 -0700

    basic layout - add style.css / largeImg component

---
+++ src/components/PlaceFood/ImgGenerator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var ImgGenerator = () => {
+  return (
+    <div>
+      <img src="https://unsplash.it/200/300"/>
+    </div>
+  )
+}
+
+export default ImgGenerator;
--- src/components/PlaceFood/presenter.js
+++ src/components/PlaceFood/presenter.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import ImgGenerator from './ImgGenerator';
 
 var PlaceFood = () => {
   return (
     <div className="mainContainer">
-      <img src={require(`../../../dist/img/placeholder.jpg`)} height='50%' width='50%'/>
+      <div className="header">
+        <img src={require(`../../../dist/img/placeholder.jpg`)} height='25%' width='25%'/>
+        <img src={require(`../../../dist/img/placeholder.jpg`)} height='25%' width='25%'/>
+        <img src={require(`../../../dist/img/placeholder.jpg`)} height='25%' width='25%'/>
+        <img src={require(`../../../dist/img/placeholder.jpg`)} height='25%' width='25%'/>
+      </div>
+      <div className="linkBox">
+        &nbsp
+      </div>
+      <div className="largePicture">
+        <ImgGenerator />
+           <form>
+          <input type="text" name="https://fpph/200"/>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
     </div>
   )
 }
