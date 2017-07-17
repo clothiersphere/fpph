dff38f8fa474be1b9ede4a1331eca43333ece94b
ChristopherLee <xchrislee@gmail.com>
2017-07-17 15:18:39 -0700

    fixed untracked files

---
+++ src/constants/apiKeys.js
@@ -0,0 +1,8 @@
+
+const unsplashAppId = 'b9578cf02cec31e31b5aa851eb61fc02ebaeda283d7964b6d8e5e182690ba7f6';
+const unsplashApiKey= '4fdb2df7817f7e0c20c2483ebe1b2a4f15567c391006867bae41120a28125456';
+
+module.exports = {
+  unsplashAppId,
+  unsplashApiKey
+};
