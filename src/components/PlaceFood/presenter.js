b25a37d82d7e8de591771dd12f10cefffa5b4713
ChristopherLee <xchrislee@gmail.com>
2017-07-16 18:08:15 -0700

    more messing with layout

--- src/components/PlaceFood/presenter.js
+++ src/components/PlaceFood/presenter.js
@@ -10,17 +10,33 @@ var PlaceFood = () => {
         <img src={require(`../../../dist/img/placeholder.jpg`)} height='25%' width='25%'/>
         <img src={require(`../../../dist/img/placeholder.jpg`)} height='25%' width='25%'/>
       </div>
-      <div className="linkBox">
-        &nbsp
-      </div>
-      <div className="largePicture">
-        <ImgGenerator />
-           <form>
-          <input type="text" name="https://fpph/200"/>
-          <input type="submit" value="Submit"/>
-        </form>
-      </div>
+      <div className="mainBody">
+        
+        <div className="linkBox">
+        <img src='https://unsplash.it/200'/>
+          <div className="firstBox">
+            <form>
+              <input type="text" name="textBox" className="textBox" value="https://fpph.com/200"/>
+             
+            </form>
+          </div>
+          <br/>
+          <div className="secondBox">
+            <form>
+              <input type="text" name="textBox" className="textBox"value="https://fpph.com/200/300"/>
+             
+            </form>
+          </div>
+        </div>
+        <div className="largePicture">
+          <ImgGenerator />
+          <form>
+            <input type="text" name="textBox" className="textBox2"/>
+            <input type="submit" value="Submit"/>
+          </form>
+        </div>
     </div>
+  </div>
   )
 }
 
