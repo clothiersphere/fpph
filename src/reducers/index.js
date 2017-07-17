d8d83afda97b441ba37518ecd8a7d7afe94a8ecf
ChristopherLee <xchrislee@gmail.com>
2017-07-17 16:06:53 -0700

    Add image reducer to reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import image from './image';
+
 export default combineReducers({
+  image, 
   routing: routerReducer
 })
