20c99c84aee9722568cd14dcd0fd35bd3cb93cfc
ChristopherLee <xchrislee@gmail.com>
2017-07-17 16:04:52 -0700

    Image reducer created

---
+++ src/reducers/track.js
@@ -0,0 +1,16 @@
+import * as actionTypes from '../constants/actionTypes';
+
+const initialState = [];
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.IMAGES_SET:
+      return setImages(state, action);
+  }
+  return state;
+}
+
+function setImages(state, action) {
+  const { images } = action;
+  return [...state, ...tracks ];
+}
